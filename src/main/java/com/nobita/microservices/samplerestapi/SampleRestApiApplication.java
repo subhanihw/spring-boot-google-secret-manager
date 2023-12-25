aa3fae6a30b2c78de191fb66590b4f60b0f0ccea
subhanihw <subhanihw29@gmail.com>
2023-12-25 15:41:57 +0530

    Database

--- src/main/java/com/nobita/microservices/samplerestapi/SampleRestApiApplication.java
+++ src/main/java/com/nobita/microservices/samplerestapi/SampleRestApiApplication.java
@@ -2,8 +2,9 @@ package com.nobita.microservices.samplerestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SampleRestApiApplication {
 
     public static void main(String[] args) {
