854570175527edcca2cc4c85317d4e65fb298e46
subhanihw <subhanihw29@gmail.com>
2023-12-25 20:39:21 +0530

    Database

--- src/main/java/com/nobita/microservices/samplerestapi/restcontroller/StudentController.java
+++ src/main/java/com/nobita/microservices/samplerestapi/restcontroller/StudentController.java
@@ -24,7 +24,7 @@ public class StudentController {
 
     @GetMapping("/")
     public List<Student> getAllStudent() {
-        return (List<Student>) service.getAllStudents();
+        return service.getAllStudents();
     }
 
     @GetMapping("/{id}")
